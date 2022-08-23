6aeb7acccfd6c9517b4898b7ec0c4611490499bc
Hatton <hattonjohn@gmail.com>
2022-08-23 14:37:44 -0600

    fix: improve option descriptions

--- src/cli.ts
+++ src/cli.ts
@@ -14,41 +14,44 @@ program
   .usage('<comma separated list of urls> [options]')
   .argument('<urls>', 'comma-separated urls to start generating PDF from')
   .showHelpAfterError(true)
+  .option('--outputPath <filename>', 'path to the output PDF file', 'site.pdf')
   .option(
     '--excludeURLs <urls>',
-    'urls to be excluded in PDF',
+    'URLs to be excluded in PDF. Comma separated.',
     commaSeparatedList,
     [],
   )
   .option(
     '--contentSelector <selector>',
-    'used to find the part of main content',
+    'CSS selector for the major sections. Default is `article`.',
     'article',
   )
+  .option('--pageSize <format>', 'PDF page size ex: A3, A4...')
+  .option(
+    '--coverPath <string>',
+    'file path to HTML snippet cover (see README)',
+  )
+  .option('--disableTOC', 'disable table of contents')
   .option(
     '--nextPageSelector <selector>',
-    'used to find next url',
+    'CSS selector for the link to follow to the next section',
     'a.pagination-nav__link--next',
   )
   .option(
     '--excludeSelectors <selectors>',
-    'exclude selector ex: .nav',
+    'CSS selector for HTML elements. E.g. .nav,.sidebar',
     commaSeparatedList,
     ['.theme-doc-breadcrumbs,a.theme-edit-this-page'],
   )
   .option(
     '--cssStyle <cssString>',
-    'css style to adjust PDF output ex: body{padding-top: 0;}',
+    'Apply to PDF output, e.g. body{padding-top: 0;} . Normally you would instead use @media print {...} in your Docusaurus custom.css',
   )
-  .option('--outputPath <filename>', 'path to the output PDF file', 'site.pdf')
   .option(
     '--pdfMargin <margin>',
     'set margin around PDF file',
     generatePuppeteerPDFMargin,
   )
-  .option('--pageSize <format>', 'pdf page size ex: A3, A4...')
-  .option('--coverPath <string>', 'path to HTML snippet cover (see README)')
-  .option('--disableTOC', 'disable table of contents')
   .option('--waitForRender <timeout>', 'wait for document render')
   .option('--headerTemplate <html>', 'html template for page header')
   .option('--footerTemplate <html>', 'html template for page footer')
